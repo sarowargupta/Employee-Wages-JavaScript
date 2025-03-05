ecd5bde19e26d259b6267de1dca23e8bdd1b3ed2
sarowargupta <sarowargupta2001@gmail.com>
2025-03-05 22:07:35 +0530

    [Sarowar Gupta]add. Store the Day and the Daily Wage along with the total wage.

--- Employee_Wages/main.js
+++ Employee_Wages/main.js
@@ -1,4 +1,4 @@
-//UC-07
+//UC-08
 const WAGE_PER_HOUR = 20;
 const FULL_TIME_HOURS = 8;
 const PART_TIME_HOURS = 4;
@@ -18,49 +18,29 @@ function getWorkHours(workType) {
 
 let totalWorkHours = 0;
 let totalDaysWorked = 0;
-let dailyWages = []; 
-let dailyWorkHours = []; 
+let dailyWageMap = new Map(); 
 
 while (totalDaysWorked < MAX_WORKING_DAYS && totalWorkHours < MAX_WORKING_HOURS) {
     let workType = Math.floor(Math.random() * 3); 
     let workHours = getWorkHours(workType);
 
     if (totalWorkHours + workHours > MAX_WORKING_HOURS) {
-        workHours = MAX_WORKING_HOURS - totalWorkHours; 
+        workHours = MAX_WORKING_HOURS - totalWorkHours;
     }
 
     let dailyWage = workHours * WAGE_PER_HOUR;
-    dailyWages.push(dailyWage); 
-    dailyWorkHours.push(workHours); 
-
     totalWorkHours += workHours;
     totalDaysWorked++;
+
+    dailyWageMap.set(totalDaysWorked, dailyWage);
 }
 
-let totalWage = dailyWages.reduce((sum, wage) => sum + wage, 0);
+let totalWage = Array.from(dailyWageMap.values()).reduce((sum, wage) => sum + wage, 0);
+
+console.log("Day-wise Wages (Stored in Map):");
+console.log(dailyWageMap);
+console.log(`Total Days Worked: ${totalDaysWorked}`);
+console.log(`Total Hours Worked: ${totalWorkHours}`);
 console.log(`Total Monthly Wage: $${totalWage}`);
 
-let dailyWageWithDay = dailyWages.map((wage, index) => `Day ${index + 1}: $${wage}`);
-console.log("Daily Wage with Day:");
-console.log(dailyWageWithDay);
-
-let fullTimeDays = dailyWages
-    .map((wage, index) => ({ day: index + 1, wage }))
-    .filter(day => day.wage === FULL_TIME_HOURS * WAGE_PER_HOUR)
-    .map(day => `Day ${day.day}`);
-
-console.log("Days when Full Time Wage ($160) was earned:");
-console.log(fullTimeDays);
-
-let firstFullTimeDay = dailyWages.find(wage => wage === FULL_TIME_HOURS * WAGE_PER_HOUR);
-console.log(`First Full Time Wage Day: $${firstFullTimeDay}`);
-
-let allFullTime = dailyWages.every(wage => wage === FULL_TIME_HOURS * WAGE_PER_HOUR);
-console.log(`Every Day is Full Time: ${allFullTime}`);
-
-let hasPartTime = dailyWages.some(wage => wage === PART_TIME_HOURS * WAGE_PER_HOUR);
-console.log(`Any Part Time Wage Present: ${hasPartTime}`);
-
-let workedDays = dailyWages.filter(wage => wage > 0).length;
-console.log(`Total Days Employee Worked: ${workedDays}`);
 
