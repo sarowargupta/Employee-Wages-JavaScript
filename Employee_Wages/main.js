c4092b8753f581b8496d6957daf6e0ffcfd81937
sarowargupta <sarowargupta2001@gmail.com>
2025-03-05 21:26:57 +0530

    [Sarowar Gupta]add. Calculate Daily Employee Wage

--- Employee_Wages/main.js
+++ Employee_Wages/main.js
@@ -1,9 +1,27 @@
-const EMPLOYEE_PRESENT = 1;
+//UC-02
+const WAGE_PER_HOUR = 20; 
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
+const NO_WORK_HOURS = 0;
 
-let attendance = Math.floor(Math.random() * 2);
+let empCheck = Math.floor(Math.random() * 3); 
+let empHours = 0;
 
-if (attendance === EMPLOYEE_PRESENT) {
-    console.log("Employee is Present");
-} else {
-    console.log("Employee is Absent");
+
+switch (empCheck) {
+    case 1:
+        empHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        empHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        empHours = NO_WORK_HOURS;
+        console.log("Employee did not work");
+        break;
 }
+
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("Employee Wage: $" + empWage);
